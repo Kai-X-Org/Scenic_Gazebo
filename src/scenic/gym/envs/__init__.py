e63ceff82ec834afd5985396970a84c6ec2da501
Kai Xu <k.px@berkeley.edu>
2024-12-15 01:01:57 -0800

    added __init__files to envs dir

--- src/scenic/gym/envs/__init__.py
+++ src/scenic/gym/envs/__init__.py
@@ -1,9 +1,3 @@
 from scenic.gym.envs.scenic_gym import ScenicGymEnv
 from scenic.gym.envs.scenic_oai_15_gym import ScenicOAI15GymEnv
 from scenic.gym.envs.scenic_oai_gym import ScenicOAIGymEnv
-from scenic.simulators.crowd_sim.simulator import CrowdSimSimulator
-from gym import register
-
-
-# register()
-
