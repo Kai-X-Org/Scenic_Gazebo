8755991ba629f1cc1143157f30d9c806703a5a14
Kai Xu <k.px@berkeley.edu>
2024-12-15 00:51:08 -0800

    changed gym file structure slightly

--- src/scenic/gym/__init__.py
+++ src/scenic/gym/__init__.py
@@ -1,6 +1,8 @@
-from scenic.gym.scenic_gym import ScenicGymEnv
-from scenic.gym.scenic_oai_15_gym import ScenicOAI15GymEnv
-from scenic.gym.scenic_oai_gym import ScenicOAIGymEnv
+from scenic.gym.envs.scenic_gym import ScenicGymEnv
+from scenic.gym.envs.scenic_oai_15_gym import ScenicOAI15GymEnv
+from scenic.gym.envs.scenic_oai_gym import ScenicOAIGymEnv
+from scenic.simulators.crowd_sim.simulator import CrowdSimSimulator
 
+# gym.env.register(
 
-
+# )
