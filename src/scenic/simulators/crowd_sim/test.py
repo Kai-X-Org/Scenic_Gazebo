7b4de939afe3f4f67e6370bcf5d31a5a331459f2
Kai Xu <k.px@berkeley.edu>
2024-12-14 16:10:26 -0800

    should be ready for testing

--- src/scenic/simulators/crowd_sim/test.py
+++ src/scenic/simulators/crowd_sim/test.py
@@ -1,7 +1,7 @@
 from scenic.gym import ScenicGymEnv
 import scenic
 from scenic.simulators.crowd_sim.simulator import CrowdSimSimulator 
-import numpy as np
+# import numpy as np
 
 scenario = scenic.scenarioFromFile("/Users/kxu/Scenic/src/scenic/simulators/crowd_sim/test.scenic",
                                    model="scenic.simulators.crowd_sim.model") # shouldn't use 2D mode?
